11a8fc47f574f194a7ae7b8b98001f6143ba4cf1
David Arnold <david.arnold@iohk.io>
2023-04-04 19:14:42 -0500

    fix: darwin build

--- nix/app/package.nix
+++ nix/app/package.nix
@@ -1,8 +1,10 @@
 let
   crane = inputs.crane.lib.overrideToolchain inputs.cells.repo.rust.toolchain;
+  inherit (inputs.nixpkgs) stdenv lib;
 in {
   default = cell.package.mdbook-paisano-preprocessor;
   mdbook-paisano-preprocessor = crane.buildPackage {
+    buildInputs = with inputs.nixpkgs; [libiconv];
     src = inputs.std.incl inputs.self [
       "src"
       "assets"
